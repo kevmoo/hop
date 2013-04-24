3e0c12d44399424f95d594cac36c12e75e0d0cc5
Kevin Moore <kevin@thinkpixellab.com>
2013-04-24 16:03:23 -0400

    hop_tasks: removed enable-type-checks flag
    Thanks, Kathy
    https://plus.google.com/105904968710974272643/posts/BnHt67xKyKL

--- lib/src/hop_tasks/dart_analyzer.dart
+++ lib/src/hop_tasks/dart_analyzer.dart
@@ -5,7 +5,6 @@ part of hop_tasks;
 //              are faster
 
 const _verboseArgName = 'verbose';
-const _enableTypeChecksArgName = 'enable-type-checks';
 const _formatMachineArgName = 'format-machine';
 
 /**
@@ -16,15 +15,14 @@ Task createDartAnalyzerTask(dynamic delayedFileList) {
   return new Task.async((context) {
     final parseResult = context.arguments;
 
-    final bool enableTypeChecks = parseResult[_enableTypeChecksArgName];
     final bool verbose = parseResult[_verboseArgName];
     final bool formatMachine = parseResult[_formatMachineArgName];
 
     return getDelayedResult(delayedFileList)
         .then((List<String> files) {
           final fileList = files.map((f) => new Path(f)).toList();
-          return _processAnalyzerFile(context, fileList, enableTypeChecks,
-              verbose, formatMachine);
+          return _processAnalyzerFile(context, fileList, verbose,
+              formatMachine);
         });
   },
   description: 'Run "dart_analyzer" for the provided dart files.',
@@ -33,16 +31,14 @@ Task createDartAnalyzerTask(dynamic delayedFileList) {
 
 void _analyzerParserConfig(ArgParser parser) {
   parser
-    ..addFlag(_enableTypeChecksArgName, abbr: 't', defaultsTo: false,
-        help: 'Generate runtime type checks')
     ..addFlag(_verboseArgName, abbr: 'v', defaultsTo: false,
         help: 'verbose output of all errors')
     ..addFlag(_formatMachineArgName, abbr: 'm', defaultsTo: false,
         help: 'Print errors in a format suitable for parsing');
 }
 
-Future<bool> _processAnalyzerFile(TaskContext context, List<Path> analyzerFilePaths,
-    bool enableTypeChecks, bool verbose, bool formatMachine) {
+Future<bool> _processAnalyzerFile(TaskContext context,
+    List<Path> analyzerFilePaths, bool verbose, bool formatMachine) {
 
   int errorsCount = 0;
   int passedCount = 0;
@@ -50,7 +46,7 @@ Future<bool> _processAnalyzerFile(TaskContext context, List<Path> analyzerFilePa
 
   return Future.forEach(analyzerFilePaths, (Path path) {
     final logger = context.getSubLogger(path.toString());
-    return _analyzer(logger, path, enableTypeChecks, verbose, formatMachine)
+    return _analyzer(logger, path, verbose, formatMachine)
         .then((int exitCode) {
 
           String prefix;
@@ -83,8 +79,8 @@ Future<bool> _processAnalyzerFile(TaskContext context, List<Path> analyzerFilePa
     });
 }
 
-Future<int> _analyzer(TaskLogger logger, Path filePath, bool enableTypeChecks,
-    bool verbose, bool formatMachine) {
+Future<int> _analyzer(TaskLogger logger, Path filePath, bool verbose,
+    bool formatMachine) {
   TempDir tmpDir;
 
   return TempDir.create()
@@ -92,10 +88,6 @@ Future<int> _analyzer(TaskLogger logger, Path filePath, bool enableTypeChecks,
         tmpDir = td;
 
         var processArgs = ['--extended-exit-code', '--work', tmpDir.dir.path];
-
-        if (enableTypeChecks) {
-          processArgs.add('--enable_type_checks');
-        }
 
         if(formatMachine) {
           processArgs.add('--machine');
