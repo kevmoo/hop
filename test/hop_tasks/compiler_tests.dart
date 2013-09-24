fc151e48ad3da9d49c57a69243a63da6510390a6
Kevin Moore <kevin@thinkpixellab.com>
2013-09-24 12:10:33 -0300

    fixed compiler test for dart2js changes

--- test/hop_tasks/compiler_tests.dart
+++ test/hop_tasks/compiler_tests.dart
@@ -86,6 +86,7 @@ Set<String> _getOutputFiles(List<String> inputFiles, CompilerTargetType type,
         outputFiles.add(newName);
         outputFiles.add(newName + '.deps');
         outputFiles.add(newName + '.map');
+        outputFiles.add(pathos.join(inFileDir, 'precompiled.js'));
       } else {
         assert(type == CompilerTargetType.DART);
         assert(inFile.endsWith('.dart'));
