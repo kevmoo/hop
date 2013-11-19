8f55c281ab3254acba2f64086d6b411f26b42b4b
Kevin Moore <kevin@thinkpixellab.com>
2013-11-19 09:20:30 -0800

    validate task completion in chained tests

--- test/hop/chain_tasks_tests.dart
+++ test/hop/chain_tasks_tests.dart
@@ -7,9 +7,11 @@ import '../test_util.dart';
 void main() {
     test('chain tasks - success', () {
 
-    var t1 = _getTask('t1');
-    var t2 = _getTask('t2');
-    var t3 = _getTask('t3');
+    var log = [];
+
+    var t1 = _getTask('t1', log);
+    var t2 = _getTask('t2', log);
+    var t3 = _getTask('t3', log);
 
     var task = t1.chain("t1").
         and("t2", t2).
@@ -20,14 +22,17 @@ void main() {
     return runTaskInTestRunner(task)
         .then((RunResult result) {
           expect(result.success, isTrue);
+          expect(log, ['t1', 't2', 't3']);
         });
   });
 
   test('chain tasks - fail', () {
 
-    var t1 = _getTask('t1', false);
-    var t2 = _getTask('t2');
-    var t3 = _getTask('t3');
+    var log = [];
+
+    var t1 = _getTask('t1', log, false);
+    var t2 = _getTask('t2', log);
+    var t3 = _getTask('t3', log);
 
     var task = t1.chain("t1").
         and("t2", t2).
@@ -38,14 +43,17 @@ void main() {
     return runTaskInTestRunner(task)
         .then((RunResult result) {
           expect(result.exitCode, RunResult.FAIL.exitCode);
+          expect(log, []);
         });
   });
 
   test('chain tasks - error', () {
 
-    var t1 = _getTask('t1');
-    var t2 = _getTask('t2', null);
-    var t3 = _getTask('t3');
+    var log = [];
+
+    var t1 = _getTask('t1', log);
+    var t2 = _getTask('t2', log, null);
+    var t3 = _getTask('t3', log);
 
     var task = t1.chain("t1").
         and("t2", t2).
@@ -56,13 +64,15 @@ void main() {
     return runTaskInTestRunner(task)
         .then((RunResult result) {
           expect(result.exitCode, RunResult.EXCEPTION.exitCode);
+          expect(log, ['t1']);
         });
   });
 }
 
-Task _getTask(String str, [bool shouldSucceed = true]) {
+Task _getTask(String str, List<String> log, [bool shouldSucceed = true]) {
   return new Task((TaskContext ctx) {
     if(shouldSucceed == null) throw 'foo!';
     if(shouldSucceed == false) ctx.fail("fail!");
+    log.add(str);
   });
 }
