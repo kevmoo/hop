8e5a1f84aa60bfbea741880d3e2d5f835cd38fd7
Kevin Moore <kevin@thinkpixellab.com>
2013-11-26 10:14:43 -0600

    added a test to validate throw on missing chained task reference

--- test/hop/chain_tasks_tests.dart
+++ test/hop/chain_tasks_tests.dart
@@ -5,7 +5,7 @@ import 'package:unittest/unittest.dart';
 import '../test_util.dart';
 
 void main() {
-    test('chain tasks - success', () {
+  test('chain tasks - success', () {
 
     var log = [];
 
@@ -20,6 +20,27 @@ void main() {
     expect(task is Task, isTrue);
 
     return runTaskInTestRunner(task)
+        .then((RunResult result) {
+          expect(result.success, isTrue);
+          expect(log, ['t1', 't2', 't3']);
+        });
+  });
+
+  test('chain tasks, via registry', () {
+
+    var reg = new TaskRegistry();
+
+    var log = [];
+
+    reg.addTask('t1', _getTask('t1', log));
+    reg.addTask('t2', _getTask('t2', log));
+    reg.addTask('t3', _getTask('t3', log));
+
+    var chained = reg.addChainedTask('chained', ['t1', 't2', 't3']);
+
+    expect(chained is Task, isTrue);
+
+    return runRegistry(reg, ['chained'])
         .then((RunResult result) {
           expect(result.success, isTrue);
           expect(log, ['t1', 't2', 't3']);
@@ -59,6 +80,8 @@ void main() {
         and("t2", t2).
         and("t3", t3);
 
+    expect(task.description, 'Chained Task');
+
     expect(task is Task, isTrue);
 
     return runTaskInTestRunner(task)
@@ -66,6 +89,32 @@ void main() {
           expect(result.exitCode, RunResult.EXCEPTION.exitCode);
           expect(log, ['t1']);
         });
+  });
+
+  test('description', () {
+    var reg = new TaskRegistry();
+
+    reg.addTask('one', (ctx) {});
+    reg.addTask('two', (ctx) {});
+    reg.addTask('three', (ctx) {});
+
+    var chained1 = reg.addChainedTask('chained1', ['one', 'two', 'three']);
+    expect(chained1.description, 'Chained Task: one, two, three');
+
+    var chained2 = reg.addChainedTask('chained2', ['one', 'two', 'three'],
+        description: 'override');
+    expect(chained2.description, 'override');
+  });
+
+  test('missing task', () {
+    var reg = new TaskRegistry();
+
+    expect(() {
+      reg.addChainedTask('oops', ['nope']);
+    }, throwsA((errorObj) {
+      return errorObj.toString()
+          .contains('The task "nope" has not be registered');
+    }));
   });
 }
 
