8d806b306a3c807c1266982933352f98b4bf779e
Kevin Moore <kevin@thinkpixellab.com>
2013-11-29 13:46:59 -0600

    a bit of moving around

--- test/hop/_hop.dart
+++ test/hop/_hop.dart
@@ -1,9 +1,8 @@
 library test_hop;
 
-// TODO: test extended args
-
-import 'package:hop/hop_core.dart';
 import 'package:unittest/unittest.dart';
+import 'package:hop/src/hop_runner.dart';
+import '../test_util.dart';
 
 import 'arg_tests.dart' as args;
 import 'chain_tasks_tests.dart' as chain;
@@ -14,12 +13,14 @@ import 'simple_add_task_tests.dart' as simple_add_task;
 import 'task_list_tests.dart' as task_list;
 import 'task_result_tests.dart' as task_result;
 import 'util_tests.dart' as util;
+import 'extended_args_test.dart' as extended_args;
 
 void main() {
   group('hop', () {
     group('args', args.main);
     group('chain tasks', chain.main);
     group('dependency', dependency.main);
+    group('extended args', extended_args.main);
     group('integration', integration.main);
     group('logging', logging.main);
     group('add task', simple_add_task.main);
@@ -27,58 +28,15 @@ void main() {
     group('task result', task_result.main);
     group('util', util.main);
 
-    group('TaskArgument', () {
-
-      test('valid ctor args', () {
-        final valid = ['a', 'a-b', 'a-cool-name'];
-        for(final v in valid) {
-          expect(() => new TaskArgument(v), returnsNormally);
-        }
-
-        final invalid = ['', null, ' ', '-', 'A', 'a-', 'a-B', 'a_b', 'a ', 'a b'];
-        for(final v in invalid) {
-          expect(() => new TaskArgument(v), throwsArgumentError);
-        }
-
-        expect(() => new TaskArgument('cool', required: null), throwsArgumentError);
-        expect(() => new TaskArgument('cool', multiple: null), throwsArgumentError);
+    group('HopConfig', () {
+      test('HopConfig: registry cannot be null', () {
+        expect(() => runRegistry(null, []), throwsArgumentError);
       });
 
-      test('validate arg list', () {
-        final validate = (List<TaskArgument> args, bool isGood) {
-          final matcher = isGood ? returnsNormally : throwsArgumentError;
-          expect(() => TaskArgument.validateArgs(args), matcher);
-        };
-
-        // empty is fine
-        validate([], true);
-
-        validate(null, false);
-
-        // null arg is bad
-        validate([null], false);
-
-        // first required is fine
-        validate([new TaskArgument('a', required: true)], true);
-
-        // first required and mult is fine
-        validate([new TaskArgument('a', required: true, multiple: true)], true);
-
-        // first required, second not required is fine
-        validate([new TaskArgument('a', required: true), new TaskArgument('b', required: false)], true);
-
-        // first not required, second required is bad
-        validate([new TaskArgument('a', required: false), new TaskArgument('b', required: true)], false);
-
-        // last multiple is fine
-
-        // 'N' multiple, 'N+1' non-multiple is not fine
-        validate([new TaskArgument('a', multiple: true), new TaskArgument('b', multiple: false)], false);
-
-        // dupe names is not fine
-        validate([new TaskArgument('a'), new TaskArgument('a')], false);
+      test('HopConfig: args cannot be null', () {
+        var reg = new TaskRegistry();
+        expect(() => runRegistry(reg, null), throwsArgumentError);
       });
-
     });
   });
 }
--- test/hop/extended_args_test.dart
+++ test/hop/extended_args_test.dart
@@ -1,19 +1,57 @@
-library test.hop.sync;
+library test.hop.extended_args;
 
-import 'package:hop/src/hop_runner.dart';
 import 'package:unittest/unittest.dart';
-import '../test_util.dart';
+import 'package:hop/hop_core.dart';
 
 void main() {
 
-  // TODO: move this to a different test file
-  test('HopConfig: registry cannot be null', () {
-    expect(() => runRegistry(null, []), throwsArgumentError);
+  test('valid ctor args', () {
+    final valid = ['a', 'a-b', 'a-cool-name'];
+    for(final v in valid) {
+      expect(() => new TaskArgument(v), returnsNormally);
+    }
+
+    final invalid = ['', null, ' ', '-', 'A', 'a-', 'a-B', 'a_b', 'a ', 'a b'];
+    for(final v in invalid) {
+      expect(() => new TaskArgument(v), throwsArgumentError);
+    }
+
+    expect(() => new TaskArgument('cool', required: null), throwsArgumentError);
+    expect(() => new TaskArgument('cool', multiple: null), throwsArgumentError);
   });
 
-  // TODO: move this to a different test file
-  test('HopConfig: args cannot be null', () {
-    var reg = new TaskRegistry();
-    expect(() => runRegistry(reg, null), throwsArgumentError);
+  test('validate arg list', () {
+    final validate = (List<TaskArgument> args, bool isGood) {
+      final matcher = isGood ? returnsNormally : throwsArgumentError;
+      expect(() => TaskArgument.validateArgs(args), matcher);
+    };
+
+    // empty is fine
+    validate([], true);
+
+    validate(null, false);
+
+    // null arg is bad
+    validate([null], false);
+
+    // first required is fine
+    validate([new TaskArgument('a', required: true)], true);
+
+    // first required and mult is fine
+    validate([new TaskArgument('a', required: true, multiple: true)], true);
+
+    // first required, second not required is fine
+    validate([new TaskArgument('a', required: true), new TaskArgument('b', required: false)], true);
+
+    // first not required, second required is bad
+    validate([new TaskArgument('a', required: false), new TaskArgument('b', required: true)], false);
+
+    // last multiple is fine
+
+    // 'N' multiple, 'N+1' non-multiple is not fine
+    validate([new TaskArgument('a', multiple: true), new TaskArgument('b', multiple: false)], false);
+
+    // dupe names is not fine
+    validate([new TaskArgument('a'), new TaskArgument('a')], false);
   });
 }
