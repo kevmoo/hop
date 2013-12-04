3609cf436603ca8dd763cafb140164cb7a9aa6c8
Kevin Moore <kevin@thinkpixellab.com>
2013-12-04 14:26:24 -0600

    notes on getPlatformBin

--- lib/src/tasks_shared.dart
+++ lib/src/tasks_shared.dart
@@ -2,6 +2,13 @@ library hop_tasks.shared;
 
 import 'dart:io';
 
+/**
+ * Used by scripts which invoke the build-in Dart commands.
+ *
+ * On windows, some commands end with '.bat'.
+ *
+ * This handles things transparently.
+ */
 String getPlatformBin(String binName) {
   if(Platform.operatingSystem == 'windows') {
     return '${binName}.bat';
