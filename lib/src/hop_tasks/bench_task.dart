2a45e89aa316eedf38a4efcd454db4ee3c3a692b
Kevin Moore <github@j832.com>
2014-03-04 20:53:34 -0800

    cleanup to bench

--- lib/src/hop_tasks/bench_task.dart
+++ lib/src/hop_tasks/bench_task.dart
@@ -19,70 +19,66 @@ const _COMMAND_ARG = 'command';
 const String _RUN_COUNT_ARE_NAME = 'run-count';
 
 Task createBenchTask() => new Task((TaskContext ctx) {
-    final parseResult = ctx.arguments;
+  var parseResult = ctx.arguments;
 
-    final count = int.parse(parseResult[_RUN_COUNT_ARE_NAME],
-        onError: (s) => _DEFAULT_RUN_COUNT);
+  var count = int.parse(parseResult[_RUN_COUNT_ARE_NAME],
+      onError: (s) => _DEFAULT_RUN_COUNT);
 
-    List<String> commandParams = ctx.extendedArgs[_COMMAND_ARG];
+  List<String> commandParams = ctx.extendedArgs[_COMMAND_ARG];
 
-    String processName = commandParams.first;
-    final args = commandParams.sublist(1);
+  String processName = commandParams.first;
+  var args = commandParams.sublist(1);
 
-    return _runMany(ctx, count, processName, args)
-        .then((list) {
-          final values = list.map((brr) => brr.executionDurationMilliseconds);
-          final stats = new _Stats(values);
-          print(stats.toString());
-        });
+  return _runMany(ctx, count, processName, args).then((list) {
+    var values = list.map((brr) => brr.executionDurationMilliseconds);
+    var stats = new _Stats(values);
+    print(stats.toString());
+  });
 
-  },
-  argParser: _benchParserConfig(),
-  description: 'Run a benchmark against the provided task',
-  extendedArgs: [new TaskArgument('command', required: true, multiple: true)]);
+},
+    argParser: _benchParserConfig(),
+    description: 'Run a benchmark against the provided task',
+    extendedArgs:
+      [new TaskArgument('command', required: true, multiple: true)]);
 
-ArgParser _benchParserConfig() =>
-    new ArgParser()
-      ..addOption(_RUN_COUNT_ARE_NAME, abbr: 'r',
-          defaultsTo: _DEFAULT_RUN_COUNT.toString(),
-          help: 'Specify the number times the specified command should be run');
+ArgParser _benchParserConfig() => new ArgParser()..addOption(
+    _RUN_COUNT_ARE_NAME, abbr: 'r', defaultsTo: _DEFAULT_RUN_COUNT.toString(),
+    help: 'Specify the number times the specified command should be run');
 
 Future<List<_BenchRunResult>> _runMany(TaskContext ctx, int count,
     String processName, List<String> args) {
 
   assert(count > 1);
-  final countStrLength = count.toString().length;
+  var countStrLength = count.toString().length;
 
-  final range = new Iterable.generate(count, (i) => i);
-  final results = new List<_BenchRunResult>();
+  var range = new Iterable.generate(count, (i) => i);
+  var results = new List<_BenchRunResult>();
 
   return Future.forEach(range, (i) {
-    return _runOnce(i+1, processName, args)
-        .then((result) {
-          final paddedNumber = Util.padLeft(result.runNumber.toString(), countStrLength);
-          ctx.fine("Test $paddedNumber of $count - ${result.executionDuration}");
-          results.add(result);
-        });
-    })
-    .then((_) {
-      return results;
+    return _runOnce(i + 1, processName, args).then((result) {
+      var paddedNumber = Util.padLeft(result.runNumber.toString(),
+          countStrLength);
+      ctx.fine("Test $paddedNumber of $count - ${result.executionDuration}");
+      results.add(result);
     });
+  }).then((_) {
+    return results;
+  });
 }
 
-Future<_BenchRunResult> _runOnce(int runNumber, String processName, List<String> args) {
-  var watch = new Stopwatch()
-    ..start();
+Future<_BenchRunResult> _runOnce(int runNumber, String processName,
+    List<String> args) {
+  var watch = new Stopwatch()..start();
 
   int postStartMicros;
 
-  return Process.start(processName, args)
-      .then((process) {
-        postStartMicros = watch.elapsedMicroseconds;
-        return pipeProcess(process);
-      })
-      .then((int exitCode) {
-        return new _BenchRunResult(runNumber, exitCode == 0, postStartMicros, watch.elapsedMicroseconds);
-      });
+  return Process.start(processName, args).then((process) {
+    postStartMicros = watch.elapsedMicroseconds;
+    return pipeProcess(process);
+  }).then((int exitCode) {
+    return new _BenchRunResult(runNumber, exitCode == 0, postStartMicros,
+        watch.elapsedMicroseconds);
+  });
 }
 
 class _BenchRunResult {
@@ -91,11 +87,14 @@ class _BenchRunResult {
   final int postEndMicroseconds;
   final bool completed;
 
-  _BenchRunResult(this.runNumber, this.completed, this.postStartMicroseconds, this.postEndMicroseconds);
+  _BenchRunResult(this.runNumber, this.completed, this.postStartMicroseconds,
+      this.postEndMicroseconds);
 
-  int get executionDurationMilliseconds => postEndMicroseconds - postStartMicroseconds;
+  int get executionDurationMilliseconds =>
+      postEndMicroseconds - postStartMicroseconds;
 
-  Duration get executionDuration => new Duration(microseconds: executionDurationMilliseconds);
+  Duration get executionDuration =>
+      new Duration(microseconds: executionDurationMilliseconds);
 
   @override
   String toString() => '''
@@ -113,54 +112,54 @@ class _Stats {
   final num standardDeviation;
   final num standardError;
 
-  _Stats.raw(int count, this.mean, this.median, this.max, this.min, num standardDeviation) :
-    this.count = count,
-    this.standardDeviation = standardDeviation,
-    standardError = standardDeviation / math.sqrt(count);
+  _Stats.raw(int count, this.mean, this.median, this.max, this.min,
+      num standardDeviation)
+      : this.count = count,
+        this.standardDeviation = standardDeviation,
+        standardError = standardDeviation / math.sqrt(count);
 
   factory _Stats(Iterable<int> source) {
     assert(source != null);
 
-    final list = source.toList()
-        ..sort();
+    var list = source.toList()..sort();
 
     assert(!list.isEmpty);
 
-    final int count = list.length;
+    var count = list.length;
 
-    final max = list.last;
-    final min = list.first;
+    var max = list.last;
+    var min = list.first;
 
-    int sum = 0;
+    var sum = 0;
 
     list.forEach((num value) {
       sum += value;
     });
 
-    final mean = sum / count;
+    var mean = sum / count;
 
     // variance
     // The average of the squared difference from the Mean
 
     num sumOfSquaredDiffFromMean = 0;
     list.forEach((num value) {
-      final squareDiffFromMean = math.pow(value - mean, 2);
+      var squareDiffFromMean = math.pow(value - mean, 2);
       sumOfSquaredDiffFromMean += squareDiffFromMean;
     });
 
-    final variance = sumOfSquaredDiffFromMean / count;
+    var variance = sumOfSquaredDiffFromMean / count;
 
     // standardDeviation: sqrt of the variance
-    final standardDeviation = math.sqrt(variance);
+    var standardDeviation = math.sqrt(variance);
 
     num median = null;
     // if length is odd, take middle value
     if (count % 2 == 1) {
-      final middleIndex = (count / 2 - 0.5).toInt();
+      var middleIndex = (count / 2 - 0.5).toInt();
       median = list[middleIndex];
     } else {
-      final secondMiddle = count ~/ 2;
-      final firstMiddle = secondMiddle - 1;
+      var secondMiddle = count ~/ 2;
+      var firstMiddle = secondMiddle - 1;
       median = (list[firstMiddle] + list[secondMiddle]) / 2.0;
     }
 
@@ -169,27 +168,25 @@ class _Stats {
 
   String toString() {
     var rows = [
-                  ['Min', min],
-                  ['Max', max],
-                  ['Media', median],
-                  ['Mean', mean],
-                  ['StdDev', standardDeviation],
-                  ['StdDev%', (standardDeviation / mean * 100).toStringAsFixed(5) + '%'],
-                  ['StdErr', standardError],
-                  ['StdDev%', (standardError / mean * 100).toStringAsFixed(5) + '%'],
-                  ];
+        ['Min', min],
+        ['Max', max],
+        ['Media', median],
+        ['Mean', mean],
+        ['StdDev', standardDeviation],
+        ['StdDev%', (standardDeviation / mean * 100).toStringAsFixed(5) + '%'],
+        ['StdErr', standardError],
+        ['StdDev%', (standardError / mean * 100).toStringAsFixed(5) + '%'],];
 
-    final cols = [
-                  new ColumnDefinition('Name', (a) => a[0]),
-                  new ColumnDefinition('Value', (a) {
-                    var val = a[1];
-                    if(val is num) {
-                      return new Duration(microseconds: val.toInt()).toString();
-                    } else {
-                      return val.toString();
-                    }
-                  })
-                  ];
+    var cols = [
+        new ColumnDefinition('Name', (a) => a[0]),
+        new ColumnDefinition('Value', (a) {
+      var val = a[1];
+      if (val is num) {
+        return new Duration(microseconds: val.toInt()).toString();
+      } else {
+        return val.toString();
+      }
+    })];
 
     return Console.getTable(rows, cols).join('\n');
   }
