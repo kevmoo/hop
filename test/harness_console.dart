d43277d7131695d39a2afc12213456034c716d33
Kevin Moore <kevmoo@google.com>
2015-04-16 08:22:36 -0700

    testing stats

--- test/harness_console.dart
+++ test/harness_console.dart
@@ -3,10 +3,12 @@ library harness_console;
 import 'package:unittest/unittest.dart';
 import 'hop/_hop.dart' as hop;
 import 'hop_tasks/_hop_tasks.dart' as hop_tasks;
+import 'stats_test.dart' as stats;
 
 void main() {
   groupSep = ' - ';
 
   group('hop', hop.main);
   group('hop_tasks', hop_tasks.main);
+  group('stats', stats.main);
 }
---
+++ test/stats_test.dart
@@ -0,0 +1,17 @@
+import 'package:unittest/unittest.dart';
+import 'package:hop/src/stats.dart';
+
+void main() {
+  test('basics', () {
+    var stats = new Stats([10, 20, 30, 40, 50]);
+
+    expect(stats.min, 10);
+    expect(stats.max, 50);
+    expect(stats.count, 5);
+    expect(stats.mean, 30);
+    expect(stats.median, 30);
+
+    expect(stats.standardDeviation, closeTo(14.1, 0.1));
+    expect(stats.standardError, closeTo(6.3, 0.1));
+  });
+}
