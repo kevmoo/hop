19d8c9b53f8fc4068e7a50c285752483cf1362f3
Kevin Moore <kevmoo@google.com>
2015-04-16 08:10:59 -0700

    move stats to its own file

--- lib/src/hop_tasks/bench_task.dart
+++ lib/src/hop_tasks/bench_task.dart
@@ -2,12 +2,12 @@ library hop_tasks.bench;
 
 import 'dart:async';
 import 'dart:io' hide Console;
-import 'dart:math' as math;
+
 import 'package:args/args.dart';
 import 'package:bot/bot.dart';
-import 'package:bot_io/bot_io.dart';
 import 'package:hop/hop_core.dart';
 import 'package:hop/src/hop_tasks/process.dart';
+import 'package:hop/src/stats.dart';
 
 // TODO: options for handling failed processes?
 // TODO: move some of the stat-related code to NumebrEnumerable?
@@ -48,7 +48,7 @@ Task createBenchTask() => new Task((TaskContext ctx) {
 
   return _runMany(ctx, count, processName, args).then((list) {
     var values = list.map((brr) => brr.executionDurationMilliseconds);
-    var stats = new _Stats(values);
+    var stats = new Stats(values);
     print(stats.toString());
   });
 },
@@ -119,95 +119,4 @@ class _BenchRunResult {
 $runNumber
 $executionDurationMilliseconds
 $completed''';
-}
-
-class _Stats {
-  final int count;
-  final num mean;
-  final num median;
-  final num max;
-  final num min;
-  final num standardDeviation;
-  final num standardError;
-
-  _Stats.raw(int count, this.mean, this.median, this.max, this.min,
-      num standardDeviation)
-      : this.count = count,
-        this.standardDeviation = standardDeviation,
-        standardError = standardDeviation / math.sqrt(count);
-
-  factory _Stats(Iterable<int> source) {
-    assert(source != null);
-
-    var list = source.toList()..sort();
-
-    assert(!list.isEmpty);
-
-    var count = list.length;
-
-    var max = list.last;
-    var min = list.first;
-
-    var sum = 0;
-
-    list.forEach((num value) {
-      sum += value;
-    });
-
-    var mean = sum / count;
-
-    // variance
-    // The average of the squared difference from the Mean
-
-    num sumOfSquaredDiffFromMean = 0;
-    list.forEach((num value) {
-      var squareDiffFromMean = math.pow(value - mean, 2);
-      sumOfSquaredDiffFromMean += squareDiffFromMean;
-    });
-
-    var variance = sumOfSquaredDiffFromMean / count;
-
-    // standardDeviation: sqrt of the variance
-    var standardDeviation = math.sqrt(variance);
-
-    num median = null;
-    // if length is odd, take middle value
-    if (count % 2 == 1) {
-      var middleIndex = (count / 2 - 0.5).toInt();
-      median = list[middleIndex];
-    } else {
-      var secondMiddle = count ~/ 2;
-      var firstMiddle = secondMiddle - 1;
-      median = (list[firstMiddle] + list[secondMiddle]) / 2.0;
-    }
-
-    return new _Stats.raw(count, mean, median, max, min, standardDeviation);
-  }
-
-  String toString() {
-    var rows = [
-      ['Min', min],
-      ['Max', max],
-      ['Media', median],
-      ['Mean', mean],
-      ['StdDev', standardDeviation],
-      ['StdDev%', (standardDeviation / mean * 100).toStringAsFixed(5) + '%'],
-      ['StdErr', standardError],
-      ['StdErr%', (standardError / mean * 100).toStringAsFixed(5) + '%'],
-    ];
-
-    var cols = [
-      new ColumnDefinition('Name', (a) => a[0]),
-      new ColumnDefinition('Value', (a) {
-        var val = a[1];
-        if (val is num) {
-          return new Duration(microseconds: val.toInt()).toString();
-        } else {
-          return val.toString();
-        }
-      })
-    ];
-
-    return Console.getTable(rows, cols).join('\n');
-  }
 }
